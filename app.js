12091ac1608c82a024115a31871c4059e535108b
Rizki Awal R <rizkiawal16@gmail.com>
2022-06-04 09:53:48 +0700

    tambah post

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 
 app.get('/', function(req, res) {
     res.json({
+        type: 'get',
         number: 1,
         nama: 'Rizki Awal Ramadhani',
         npm: '2042019'
     });
 })
+
+app.post('/cr', function(req, res) {
+    res.json({
+        type: 'post',
+        number: 2,
+        nama: 'RUcup',
+        npm: '2042000'
+    });
+})
+
 
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
