2af836e7add687a7fdb9429d0f4156fd8605363f
Rizki Awal R <rizkiawal16@gmail.com>
2022-06-04 09:04:24 +0700

    Ganti nama dan deploy

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.json({
-    number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
-  });
+app.get('/', function(req, res) {
+    res.json({
+        number: 1,
+        nama: 'Rizki Awal Ramadhani',
+        npm: '2042019'
+    });
 })
 
 app.set('port', (process.env.PORT || 8080));
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
